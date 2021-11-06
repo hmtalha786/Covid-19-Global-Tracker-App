14a66261e5e6602626411fab58789cd4cb6e5ca6
Muhammad Talha <talhaofficialwork@gmail.com>
2021-11-06 19:23:04 +0500

    Finalized Header UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './Components/Header';
+import Global from './Components/Global';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <Header />
+      <Global/>
+      <div className="App">Hello World</div>
+    </>
   );
 }
 
